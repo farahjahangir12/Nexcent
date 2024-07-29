16aa2bb06a6332fdc65a554a44575901f8be1e92
Farah Jahangir <141430739+farahjahangir12@users.noreply.github.com>
2024-07-29 09:58:06 -0700

    Update Testimonials.jsx

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -6,12 +6,12 @@ function Testimonials() {
         <>
             <section className="text-container mt-3 mb-4 pt-4">
                 <h1 className="fs-1 fw-bold text-center">Caring is the new marketing</h1>
-                <div className="d-flex justify-content-center">
-                    <p className="w-50 text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae tempore officia ad fugit dicta doloribus maxime    
+                <div className="d-flex justify-content-center w-50">
+                    <p className="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae tempore officia ad fugit dicta doloribus maxime    
                      deserunt recusandae in, natus dolores at, illo molestiae. Minus provident error incidunt molestiae architecto!</p>
                 </div>
                 </section>
-            <section className="d-flex flex-column flex-md-row justify-content-evenly align-items-center">
+            <section className="d-flex flex-column flex-md-row justify-content-md-evenly justify-content-center align-items-center">
                 <Cards image={image1} description="Creating streamlined safeguard processes with one ren" />
                 <Cards image={image2} description="What are your safeguarding responsibilities and how can you manage them" />
                 <Cards image={image3} description="Rewamping the membership model with Triathon Australia" />
