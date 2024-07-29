1e43b2333bba39e421c9c9ece2d210dd7f509c13
Farah Jahangir <141430739+farahjahangir12@users.noreply.github.com>
2024-07-29 10:20:27 -0700

    Update Awards.jsx

--- src/components/Awards.jsx
+++ src/components/Awards.jsx
@@ -2,9 +2,9 @@ import CountUp from "react-countup";
 
 function Awards({number,text}){
   return(
- <div className="card w-25 h-md-25 h-10 mt-4" >
+ <div className="card w-25 h-25 mt-4" >
   <div className="card-body text-center">
-  <h5 className="card-title fs-1 fw-bold">
+  <h5 className="card-title fs-2 fw-bold">
   <CountUp end={number} duration={2.75} />
   </h5>
     <p className="card-subtitle mb-2">{text}</p>  
