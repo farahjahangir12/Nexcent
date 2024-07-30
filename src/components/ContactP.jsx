309e75d045e10d43eb705683fd3d0ec0ded4f5bb
Farah Jahangir <141430739+farahjahangir12@users.noreply.github.com>
2024-07-30 05:15:56 -0700

    Update ContactP.jsx

--- src/components/ContactP.jsx
+++ src/components/ContactP.jsx
@@ -1,7 +1,20 @@
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import "../App.css"
 import Contacts from "./Contacts.jsx"
+import {useState} from 'react'
 function ContactP(){
+const [name,setName]=useState('');
+const [email,setEmail]=useState('');
+const [message,setMessage]=useState('');
+
+
+const handleSubmit=(e)=>{
+    e.preventDefault();
+    const userDetails={name,email,message};
+    console.log(userDetails)
+}
+
+
     return(
         <main>
             <h1 className="fs-1 mt-4 text-center fw-bold p-4">We will love to <span>Welcome</span> you at all our forums!</h1>
@@ -20,24 +33,27 @@ function ContactP(){
                    <p>farahjahangir12@gmail.com</p>
                   </div>
             </div>
-            <div className="container mt-4 mb-4 pt-4">
+            <form className="container mt-4 mb-4 pt-4" onSubmit={handleSubmit}>
                 <h2>Contact Us</h2>
-            <div class="mb-3">
-              <label for="exampleFormControlInput1" class="form-label">Full Name</label>
-              <input type="email" class="form-control w-75 " id="exampleFormControlInput1" placeholder="Enter your full name"/>
+            <div className="mb-3">
+              <label for="exampleFormControlInput1" className="form-label">Full Name</label>
+              <input type="text" className="form-control w-75 " id="exampleFormControlInput1" placeholder="Enter your full name" required value={name}
+               onChange={(e)=>setName(e.target.value)}/>
             </div>
             <div class="mb-3">
-              <label for="exampleFormControlInput1" class="form-label">Email address</label>
-              <input type="email" class="form-control w-75" id="exampleFormControlInput1" placeholder="name@example.com"/>
+              <label for="exampleFormControlInput1" className="form-label">Email address</label>
+              <input type="email" className="form-control w-75" id="exampleFormControlInput1" placeholder="name@example.com" required value={email}
+               onChange={(e)=>setEmail(e.target.value)}/>
             </div>
            <div class="mb-3">
-             <label for="exampleFormControlTextarea1" class="form-label">Message</label>
-             <textarea class="form-control w-75" id="exampleFormControlTextarea1" rows="3" placeholder="Enter your message.."></textarea>
+             <label for="exampleFormControlTextarea1" className="form-label">Message</label>
+             <textarea className="form-control w-75" id="exampleFormControlTextarea1" rows="3" placeholder="Enter your message.." required value={message}
+              onChange={(e)=>setMessage(e.target.value)}></textarea>
              <div class="mb-2 mt-2">
              <button className="w-25 bg-color" type="submit">Send</button>
             </div>
             </div>
-        </div>
+        </form>
         </section>
         <Contacts/>
         </main>
